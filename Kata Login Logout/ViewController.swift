a219ee16afeb544d80d852c7bbb6631469395f82
Jesus Martin Alonso <jesus.martin@aiwin.es>
2018-09-20 15:26:00 +0200

    Initial Commit

---
+++ Kata Login Logout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kata Login Logout
+//
+//  Created by Jesus Martin Alonso on 20/9/18.
+//  Copyright © 2018 Aiwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
